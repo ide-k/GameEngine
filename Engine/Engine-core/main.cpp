7c3541ef2eb5d9c80c751fe79aa7f4a2c34f8e60
ide-k <siddharthkhanna2k@gmail.com>
2020-07-17 16:13:03 -0400

    imported and initialized GLEW and added a proper bin folder
    rectangles > triangles dont @ me

--- Engine/Engine-core/main.cpp
+++ Engine/Engine-core/main.cpp
@@ -8,18 +8,26 @@ int main()
 	Window window("Engine", 960, 540);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 
 	while (!window.closed())
 	{
 		window.clear();
+#if 1
 		glBegin(GL_QUADS);
 		glVertex2d(-0.5f, -0.5f);
 		glVertex2d(-0.5f,  0.5f);
 		glVertex2d( 0.5f,  0.5f);
 		glVertex2d( 0.5f, -0.5f);
 		glEnd();
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
 		window.update();
 	}
 	
--- Engine/Engine-core/src/graphics/window.cpp
+++ Engine/Engine-core/src/graphics/window.cpp
@@ -33,6 +33,15 @@ namespace engine { namespace graphics {
 		}
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowSizeCallback(m_Window, windowResize);
+
+		std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+		if (glewInit() != GLEW_OK)
+		{
+			std::cout << "Could not initialize GlEW" << std::endl;
+			return false;
+		}
+
 		return true;
 	}
 
